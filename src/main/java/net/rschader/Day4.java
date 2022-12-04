f14c573ad638bda82248fcbfeee53d791d5a2e81
Roman Schader <me@rschader.com>
2022-12-04 14:51:19 +0100

    Fix Bug in Day4

--- src/main/java/net/rschader/Day4.java
+++ src/main/java/net/rschader/Day4.java
@@ -19,10 +19,11 @@ public class Day4 {
         public boolean fullyContains(Range range) {
             return this.from <= range.from && this.to >= range.to;
         }
-
+ 
         public boolean overlapsWith(Range range) {
-            if (this.to <= range.from && this.from <= range.to) return true;
-            return this.to >= range.from && range.to <= this.from;
+            if (this.to < range.from && this.from < range.to) return false;
+            else if (this.to > range.from && this.from > range.to) return false;
+            return true;
         }
     }
 
@@ -38,7 +39,8 @@ public class Day4 {
             String[] sections = line.split(",");
             Range range1 = getRange(sections[0]);
             Range range2 = getRange(sections[1]);
-            if (range1.fullyContains(range2) || range2.fullyContains(range1)) count++;
+            if (range1.fullyContains(range2) || range2.fullyContains(range1))
+                count++;
         }
         return count;
     }
@@ -49,7 +51,8 @@ public class Day4 {
             String[] sections = line.split(",");
             Range range1 = getRange(sections[0]);
             Range range2 = getRange(sections[1]);
-            if (range1.overlapsWith(range2)) count++;
+            if (range1.overlapsWith(range2))
+                count++;
         }
         return count;
     }
@@ -58,7 +61,7 @@ public class Day4 {
         ResourceLoader resourceLoader = new ResourceLoader();
         List<String> input = resourceLoader.load("day4.txt");
         assert (part1(input) == 431);
-        assert (part2(input) == 367);
+        assert (part2(input) == 823);
     }
 
 }
