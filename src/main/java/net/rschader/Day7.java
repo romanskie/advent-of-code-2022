55433c1220fe6e37bac7f653a2f0a742f0b9f57d
Roman Schader <me@rschader.com>
2022-12-29 13:08:06 +0100

    Solution Day 7

--- src/main/java/net/rschader/Day7.java
+++ src/main/java/net/rschader/Day7.java
@@ -6,111 +6,74 @@ import net.rschader.utils.ResourceLoader;
 
 public class Day7 {
 
-    private static int getDirSize(String dir, Map<String, Set<String>> dirToFiles,
-            Map<String, Set<String>> dirToChildren, Map<String, Integer> fileToSize) {
-        Set<String> visited = new HashSet<>();
-        Stack<String> stack = new Stack<>();
-        stack.add(dir);
-        int size = 0;
-        while (!stack.isEmpty()) {
-            String d = stack.pop();
-            visited.add(d);
-            Set<String> children = dirToChildren.getOrDefault(d, new LinkedHashSet<>());
-            Set<String> fileList = dirToFiles.getOrDefault(d, new LinkedHashSet<>());
-
-            for (String child : children) {
-                if (!visited.contains(child))
-                    stack.push(child);
-            }
-
-            for (String fileName : fileList) {
-                size += fileToSize.get(fileName);
-            }
-        }
-
-        return size;
-    }
-
-    private static int part1(List<String> input) {
-        List<String> path = new ArrayList<>();
-
-        Map<String, Set<String>> dirToChildren = new LinkedHashMap<>();
-        Map<String, Set<String>> dirToFiles = new LinkedHashMap<>();
-        Map<String, Integer> fileToSize = new LinkedHashMap<>();
+    private static Map<String, Integer> parse(List<String> input) {
+        String path = "/home";
+        Map<String, Integer> map = new HashMap<>();
 
         for (String line : input) {
-
             if (line.startsWith("$ ls"))
                 continue;
 
             else if (line.startsWith("$ cd .."))
-                path.remove(path.size() - 1);
+                path = path.substring(0, path.lastIndexOf("/"));
 
-            else if (line.startsWith("$ cd /")) {
-                String home = "/";
-                List<String> temp = new ArrayList<>();
-                temp.add(home);
-                path = temp;
-                dirToFiles.put(home, dirToFiles.getOrDefault(home, new LinkedHashSet<>()));
-                dirToChildren.put(home, dirToChildren.getOrDefault(home, new LinkedHashSet<>()));
-            }
+            else if (line.startsWith("$ cd /"))
+                path = "/home";
 
-            else if (line.startsWith("$ cd")) {
-                String direction = line.substring(5);
-                String parent = path.get(path.size() - 1);
-                path.add(direction);
-                dirToFiles.put(direction, dirToFiles.getOrDefault(direction, new LinkedHashSet<>()));
-
-                Set<String> parentDirChildren = dirToChildren.getOrDefault(parent, new LinkedHashSet<>());
-                parentDirChildren.add(direction);
-                dirToChildren.put(parent, parentDirChildren);
-            }
+            else if (line.startsWith("$ cd"))
+                path = path + "/" + line.substring(5);
 
             else if (line.startsWith("dir")) {
-                String parent = path.get(path.size() - 1);
-                Set<String> children = dirToChildren.getOrDefault(parent, new LinkedHashSet<>());
-                children.add(line.substring(4));
-                dirToChildren.put(parent, children);
+                String dirName = line.split(" ")[1];
+                map.putIfAbsent(path + "/" + dirName, 0);
             }
 
             else {
                 Integer fileSize = Integer.parseInt(line.split(" ")[0]);
-                String fileName = line.split(" ")[1];
-                fileToSize.put(fileName, fileSize);
-
-                String currentDir = path.get(path.size() - 1);
-                Set<String> fileList = dirToFiles.getOrDefault(currentDir, new LinkedHashSet<>());
-                fileList.add(fileName);
-                dirToFiles.put(currentDir, fileList);
+                String tempPath = path;
+                while (tempPath.length() > 0) {
+                    map.put(tempPath, map.getOrDefault(tempPath, 0) + fileSize);
+                    tempPath = tempPath.substring(0, tempPath.lastIndexOf("/"));
+                }
             }
 
         }
 
-        System.out.println(path);
+        return map;
+    }
 
+    private static int part1(List<String> input) {
+        Map<String, Integer> map = parse(input);
         int totalSize = 0;
-        for (String dir : dirToFiles.keySet()) {
-            int dirSize = getDirSize(dir, dirToFiles, dirToChildren, fileToSize);
+        for (String key : map.keySet()) {
+            int dirSize = map.get(key);
             if (dirSize <= 100000)
                 totalSize += dirSize;
         }
-
-        System.out.println(dirToChildren);
-        System.out.println(dirToFiles);
-
         return totalSize;
     }
-    // 1006358; 787432, 1140440
-    // 1140440, 191661, 982640, 716338 787432, 679307
 
     private static int part2(List<String> input) {
-        return -1;
+        Map<String, Integer> map = parse(input);
+        int unusedSpace = 70000000 - map.get("/home");
+        int missingSpace = 30000000 - unusedSpace;
+
+        int smallestDir = Integer.MAX_VALUE;
+        for (String key : map.keySet()) {
+            if (key.equals("/home")) continue;
+            int dirSize = map.get(key);
+            if (dirSize >= missingSpace) smallestDir = Math.min(smallestDir, dirSize);
+        }
+
+        return smallestDir;
     }
 
     public static void main(String[] args) {
         ResourceLoader resourceLoader = new ResourceLoader();
         List<String> input = resourceLoader.load("day7.txt");
-        System.out.println(part1(input));
+        System.out.println(part1(input)); // 1427048
+        System.out.println(part2(input)); // 2940614
+
     }
 
 }
